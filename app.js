eea36bbd20715c2151af5074bcde615099238b10
daviddicken <davidfromseattle@gmail.com>
2020-06-09 15:52:51 -0700

    addedfunction makeList to objects and envoked them

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var Seattle =
   maxCust : 65,
   avgCookie : 6.3,
   amStoreHours : [6, 7, 8, 9, 10, 11],
-  pmStoreHours : [12, 1, 2, 3, 4, 5, 6, 7]
+  pmStoreHours : [12, 1, 2, 3, 4, 5, 6, 7],
+  makeList : makeList
 
 }
 
@@ -18,7 +19,8 @@ var Tokyo =
   maxCust : 24,
   avgCookie : 1.2,
   amStoreHours : [6, 7, 8, 9, 10, 11],
-  pmStoreHours : [12, 1, 2, 3, 4, 5, 6, 7]
+  pmStoreHours : [12, 1, 2, 3, 4, 5, 6, 7],
+  makeList : makeList
 
 }
 
@@ -29,7 +31,8 @@ var Dubai =
   maxCust : 38,
   avgCookie : 3.7,
   amStoreHours : [6, 7, 8, 9, 10, 11],
-  pmStoreHours : [12, 1, 2, 3, 4, 5, 6, 7]
+  pmStoreHours : [12, 1, 2, 3, 4, 5, 6, 7],
+  makeList : makeList
 
 }
 
@@ -40,7 +43,8 @@ var Paris =
   maxCust : 38,
   avgCookie : 2.3,
   amStoreHours : [6, 7, 8, 9, 10, 11],
-  pmStoreHours : [12, 1, 2, 3, 4, 5, 6, 7]
+  pmStoreHours : [12, 1, 2, 3, 4, 5, 6, 7],
+  makeList : makeList
 
 }
 
@@ -51,37 +55,44 @@ var Lima =
   maxCust : 16,
   avgCookie : 4.6,
   amStoreHours : [6, 7, 8, 9, 10, 11],
-  pmStoreHours : [12, 1, 2, 3, 4, 5, 6, 7]
-
+  pmStoreHours : [12, 1, 2, 3, 4, 5, 6, 7],
+  makeList : makeList,
 }
 
-var totalCookies = 0;
+Lima.makeList();
+Paris.makeList();
+Dubai.makeList();
+Tokyo.makeList();
+Seattle.makeList();
 
-for(var i = 0; i < Lima.amStoreHours.length; i++)
+function makeList()
 {
-  var  target = document.getElementById("Lima");
+  var totalCookies = 0;
+  for(var i = 0; i < this.amStoreHours.length; i++)
+  {
+    var  target = document.getElementById(this.store);
+    var listItem = document.createElement('li');
+    var amCookieCount = Math.ceil(randomCustomer(this.minCust, this.maxCust) * this.avgCookie);
+    listItem.textContent = this.amStoreHours[i] + "am: " + amCookieCount + " cookies";
+    target.appendChild(listItem);
+    totalCookies = totalCookies + amCookieCount;
+  }
+
+  for(var i = 0; i < this.pmStoreHours.length; i++)
+  {
+    var  target = document.getElementById(this.store);
+    var listItem = document.createElement('li');
+    var pmCookieCount = Math.ceil(randomCustomer(this.minCust, this.maxCust) * this.avgCookie);
+    listItem.textContent = this.pmStoreHours[i] + "pm: " + pmCookieCount  + " cookies";
+    target.appendChild(listItem);
+    totalCookies = totalCookies + pmCookieCount;
+  }
+
+  var  target = document.getElementById(this.store);
   var listItem = document.createElement('li');
-  var amCookieCount = Math.ceil(randomCustomer(Lima.minCust, Lima.maxCust) * Lima.avgCookie);
-  listItem.textContent = Lima.amStoreHours[i] + "am: " + amCookieCount + " cookies";
+  listItem.textContent = "Total: " + totalCookies + " cookies";
   target.appendChild(listItem);
-  totalCookies = totalCookies + amCookieCount;
 }
-
-for(var i = 0; i < Lima.pmStoreHours.length; i++)
-{
-  var  target = document.getElementById("Lima");
-  var listItem = document.createElement('li');
-  var pmCookieCount = Math.ceil(randomCustomer(Lima.minCust, Lima.maxCust) * Lima.avgCookie);
-  listItem.textContent = Lima.pmStoreHours[i] + "pm: " + pmCookieCount  + " cookies";
-  target.appendChild(listItem);
-  totalCookies = totalCookies + pmCookieCount;
-}
-
-var  target = document.getElementById("Lima");
-var listItem = document.createElement('li');
-listItem.textContent = "Total: " + totalCookies + " cookies";
-target.appendChild(listItem);
-
 
 
 function randomCustomer(min, max)
